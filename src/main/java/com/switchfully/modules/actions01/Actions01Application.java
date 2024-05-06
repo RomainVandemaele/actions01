fada9bb73d1af9871575fb28634d55b26b0ea6e8
romain <romain.v@live.be>
2024-05-06 13:42:11 +0200

    controller

--- src/main/java/com/switchfully/modules/actions01/Actions01Application.java
+++ src/main/java/com/switchfully/modules/actions01/Actions01Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Actions01Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Actions01Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Actions01Application.class, args);
+    }
 
 }
---
+++ src/main/java/com/switchfully/modules/actions01/Controller.java
@@ -0,0 +1,16 @@
+package com.switchfully.modules.actions01;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("actions")
+public class Controller {
+
+    @GetMapping( produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public String getOk() {
+        return "ok";
+    }
+}
